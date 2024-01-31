bb3dff70abd6a29e0ed97b070ffb3899d1702cb7
Denis Nganga <denisnganga16@gmail.com>
2024-01-31 14:51:17 +0300

    imported the settings into the urls of the main project

--- my_site/urls.py
+++ my_site/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from  django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("my_app.urls")),
