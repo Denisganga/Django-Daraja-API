d851760aa599bbaed15eb6a403d00aeaedb64b50
Denis Nganga <denisnganga16@gmail.com>
2024-02-01 12:16:30 +0300

    defined the view function to render a template

--- my_app/views.py
+++ my_app/views.py
@@ -4,12 +4,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django_daraja.mpesa.core import MpesaClient
 
+
 def index(request):
-    cl = MpesaClient()
-    phone_number = '0115735292'
-    amount = 1
-    account_reference = 'reference'
-    transaction_desc = 'Description'
-    callback_url = 'https://api.darajambili.com/express-payment'
-    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
-    return HttpResponse(response)
+    if request.method=='POST':
+        phone_number=request.POST.get('phone_number')
+        amount=request.POST.get('amount')
+
+        if phone_number and amount:
+            cl = MpesaClient()
+            account_reference = 'Denis'
+            transaction_desc = 'purchased a product'
+            callback_url = 'https://api.darajambili.com/express-payment'
+            response = cl.stk_push(phone_number,amount,account_reference,transaction_desc,callback_url)
+            return HttpResponse(response)
+        
+    return render(request,'index.html')
--- my_site/settings.py
+++ my_site/settings.py
@@ -132,8 +132,8 @@ MPESA_ENVIRONMENT = 'sandbox'
 
 # Credentials for the daraja app
 
-MPESA_CONSUMER_KEY = ''
-MPESA_CONSUMER_SECRET = ''
+MPESA_CONSUMER_KEY = 'wihoqx5h6kOBs6672SlLBEdKmZPZr2xPjlzOn4TEYxyZ0eMP'
+MPESA_CONSUMER_SECRET = 'r3xsVwZuGvFJSGTEAgvjnlqWCZbGS7qi2Mg6V5uAzBa6r6y7aqQbCoWHmTsteauU'
 
 #Shortcode to use for transactions. For sandbox  use the Shortcode 1 provided on test credentials page
 
@@ -156,7 +156,7 @@ MPESA_SHORTCODE_TYPE = 'paybill'
 # Sandbox passkey is available on test credentials page
 # Production passkey is sent via email once you go live
 
-MPESA_PASSKEY = ''
+MPESA_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
 
 # Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
 
