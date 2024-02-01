a365e2633387d610d2a2d31a9dfcdac83c263cdc
Denis Nganga <denisnganga16@gmail.com>
2024-02-01 03:17:54 +0300

    the urls pattern of the view,settings for credentials,and modifying the views

--- my_app/views.py
+++ my_app/views.py
@@ -1,27 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django_daraja.mpesa.core import MpesaResponse
-
-@csrf_exempt
-def express_payment_callback(request):
-    if request.method == 'POST':
-        # Parse the callback data
-        mpesa_response = MpesaResponse(request.body.decode('utf-8'))
-
-        # Check if the payment was successful
-        if mpesa_response.is_successful():
-            # Payment was successful, you can update your database or trigger other processes
-            print("Payment successful. Transaction ID:", mpesa_response.transaction_id)
-        else:
-            # Payment failed or was canceled
-            print("Payment failed or canceled. Response:", mpesa_response.response_text)
-
-        # Respond to Safaricom with a success message
-        return HttpResponse("Callback received successfully")
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django_daraja.mpesa.core import MpesaClient
--- my_site/settings.py
+++ my_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_daraja',
     'my_app',
 ]
 
@@ -126,3 +127,41 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+
+MPESA_ENVIRONMENT = 'sandbox'
+
+# Credentials for the daraja app
+
+MPESA_CONSUMER_KEY = ''
+MPESA_CONSUMER_SECRET = ''
+
+#Shortcode to use for transactions. For sandbox  use the Shortcode 1 provided on test credentials page
+
+MPESA_SHORTCODE = '174379'
+
+# Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
+# This is only used on sandbox, do not set this variable in production
+# For sandbox use the Lipa na MPESA Online Shorcode provided on test credentials page
+
+MPESA_EXPRESS_SHORTCODE = '174379'
+
+# Type of shortcode
+# Possible values:
+# - paybill (For Paybill)
+# - till_number (For Buy Goods Till Number)
+
+MPESA_SHORTCODE_TYPE = 'paybill'
+
+# Lipa na MPESA Online passkey
+# Sandbox passkey is available on test credentials page
+# Production passkey is sent via email once you go live
+
+MPESA_PASSKEY = ''
+
+# Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
+
+MPESA_INITIATOR_USERNAME = 'testapi'
+
+# Plaintext password for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
+
+#MPESA_INITIATOR_SECURITY_CREDENTIAL = 'Safaricom999!*!'
