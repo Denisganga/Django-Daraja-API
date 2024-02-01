64f31728d9ec5b39917141ce265dcb5c1f6776b3
Denis Nganga <denisnganga16@gmail.com>
2024-02-01 12:43:00 +0300

    modified the settings to handle the static on the root directory,added the style static

--- my_site/settings.py
+++ my_site/settings.py
@@ -118,8 +118,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# settings.py
 
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / "subdir".
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
