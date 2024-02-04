acbf791c91d5603bcd5d569d3d750d9b76d85e65
Denis Nganga <denisnganga16@gmail.com>
2024-02-04 14:51:09 +0300

    modified the views function to include the password which is hashed by cobining timestamp,business shortcode and MPESA_SHORTCODE

--- my_app/views.py
+++ my_app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 from django_daraja.mpesa.core import MpesaClient
+from django.conf import settings
+import base64
+import hashlib
+from datetime import datetime
 
 
 def index(request):
@@ -19,10 +23,24 @@ def index(request):
 
         if phone_number and amount:
             cl = MpesaClient()
+            business_short_code=settings.MPESA_SHORTCODE
             account_reference = 'Denis'
             transaction_desc = 'purchased a product'
             callback_url = 'https://api.darajambili.com/express-payment'
-            response = cl.stk_push(phone_number,amount,account_reference,transaction_desc,callback_url)
+            timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+            data_to_hash = f"{business_short_code}{settings.MPESA_PASSKEY}".encode('utf-8') #combined according to doc
+            hashed_data = hashlib.sha256(data_to_hash).hexdigest() #here we have been hashing the data
+            password= base64.b64encode(bytes.fromhex(hashed_data)).decode('utf-8')
+
+            response = cl.stk_push(
+                phone_number,
+                amount,
+                business_short_code,
+                account_reference,
+                transaction_desc,
+                callback_url,
+                timestamp,
+                password)
             return HttpResponse(response)
         
     return render(request,'index.html')
